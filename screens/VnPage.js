281b8fb04a6e8be0510466e49692f8c8d176a270
rajatranjan <rjt.ranjan@gmail.com>
2021-11-17 11:31:20 +0530

    web is showing in webview

--- screens/VnPage.js
+++ screens/VnPage.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
+
+const GOOGLE = "https://www.google.com/";
 
 const VnPage = ({ route, navigation }) => {
   const { itemId, otherParam } = route.params;
+  const window = useWindowDimensions();
+  const h = window.height;
+  const w = window.width;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>This is Visal Novel Page.</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <View
+        style={{
+          borderColor: "red",
+          // marginRight: 300,
+          width: window.width,
+          height: window.height,
+          // rotation: 90,
+          flex: 1,
+        }}
+      >
+        <WebView
+          source={{ uri: GOOGLE }}
+          onLoad={console.log("onLoad", h, w)}
+          scalesPageToFit={true}
+        />
+      </View>
     </View>
   );
 };
 
 export default VnPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
